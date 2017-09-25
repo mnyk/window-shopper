94fac7289298ccc70ed59c355d61de66b527e424
mnyk <nykielmat@gmail.com>
2017-09-25 17:36:58 +0200

    full working

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,53 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by mateusz on 25.09.2017.
+//  Copyright © 2017 mateusz. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class CurrencyTxtField: UITextField {
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2 , width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.790527597, green: 0.790527597, blue: 0.790527597, alpha: 0.8021992723)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+        }
+    
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        clipsToBounds = true
+        
+        if let p  = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+}
